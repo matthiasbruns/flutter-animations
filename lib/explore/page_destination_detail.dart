2709bf3bd3c9be4419943bd1f4d274a8341fccfa
Matthias Bruns <info@matthiasbruns.com>
2019-01-21 15:51:04 +0100

    Add hero

--- lib/explore/page_destination_detail.dart
+++ lib/explore/page_destination_detail.dart
@@ -4,16 +4,24 @@ import 'package:flutter_animations/explore/section.dart';
 class DestinationDetailPage extends StatelessWidget {
   final Destination destination;
 
-  const DestinationDetailPage({Key key, @required this.destination}) : super(key: key);
+  const DestinationDetailPage({Key key, @required this.destination})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Hero(
-          tag: "hero_${destination.tag}",
-          child: Image.network(
-              "${destination.imageUrl}?dl&fit=crop&crop=entropy&w=480"),
+        child: SizedBox.fromSize(
+          size: Size.fromHeight(300),
+          child: Hero(
+            tag: "hero_${destination.tag}",
+            child: FittedBox(
+              alignment: Alignment.center,
+              fit: BoxFit.cover,
+              child: Image.network(
+                  "${destination.imageUrl}?dl&fit=crop&crop=entropy&w=480"),
+            ),
+          ),
         ),
       ),
     );
