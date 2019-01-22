5c634b9f7c4f5e728ee72d743c740c08fa0f9519
Matthias Bruns <info@matthiasbruns.com>
2019-01-22 15:55:17 +0100

    Add gallery

--- lib/explore/page_destination_gallery.dart
+++ lib/explore/page_destination_gallery.dart
@@ -11,6 +11,7 @@ class DestinationGalleryPage extends StatelessWidget {
       {Key key, @required this.destination, this.initialPage: 0})
       : _galleryPageController = PageController(
           initialPage: initialPage,
+          viewportFraction: 0.8,
         ),
         super(key: key);
 
@@ -18,16 +19,19 @@ class DestinationGalleryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var itemCount = 3;
     return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: PageView.builder(
-          controller: _galleryPageController,
-          itemBuilder: (context, index) {
-            return Image.network(
-                "${destination.galleryUrls[index]}?dl&fit=crop&crop=entropy&w=480");
-          },
-          itemCount: itemCount,
-        ),
+      child: PageView.builder(
+        controller: _galleryPageController,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 0.0,
+              horizontal: 8.0,
+            ),
+            child: Image.network(
+                "${destination.galleryUrls[index]}?dl&fit=crop&crop=entropy&w=480"),
+          );
+        },
+        itemCount: itemCount,
       ),
     );
   }
